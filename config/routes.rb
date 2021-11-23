235dc990ef3885b2ab460b29a39b55e99b857cc4
obusto <ootsubo.sena.infratop@gmail.com>
2021-11-23 11:34:45 +0000

    [Add]controller&view&bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
+  root to: 'homes#top'
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :show, :edit]
 end
