aaf6e99915a6f5f65d2d74965677c42f0bb68c3f
obusto <ootsubo.sena.infratop@gmail.com>
2021-11-23 12:09:47 +0000

    [Add]header

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
+  get 'home/about' => 'homes#about'
+  
   devise_for :users
   resources :users, only: [:index, :show, :edit]
   resources :books, only: [:index, :show, :edit]
