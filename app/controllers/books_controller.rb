7e85611c3d1bfd36dcdd9b9fc57e6a256de1c91c
obusto <ootsubo.sena.infratop@gmail.com>
2021-11-24 07:36:59 +0000

    [Add]update,destroy

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,15 @@
 class BooksController < ApplicationController
   def new
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
     redirect_to book_path(@book.id)
   end
-  
-  
+
+
   def index
     @books = Book.all
     @book = Book.new
@@ -21,16 +21,27 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = User.find(current_user.id)
   end
-  
+
   def edit
     @book = Book.find(params[:id])
   end
-  
-  
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
   private
 
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
